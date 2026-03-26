2a0ec2627f52b8ed208832d71c8f8e893d6d6155
Pankaj Chitra <pankajchitra17@gmail.com>
2026-03-26 22:08:34 +0530

    updated schema for user and notes

--- models/notemodule.js
+++ models/notemodule.js
@@ -1,49 +1,53 @@
-// import mongoose from 'mongoose';
-// const { Schema, model } = mongoose;
-
-
-// const MySchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     maxlength: 50
-//   },
-//   age : {
-//     type: Number,
-//     required: true,
-//   },
-//   weight : {
-//     type: Number,
-//     required: true,
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
-// const userModel = model('User', MySchema);
-
-// export default userModel;
-
 import mongoose from 'mongoose';
-const { Schema, model, models } = mongoose;
+const { Schema, model } = mongoose;
 
-const NoteSchema = new Schema({
- name: {
+
+const MySchema = new Schema({
+  name: {
     type: String,
     required: true,
     maxlength: 50
   },
-  age : {
-    type: Number,
-    required: true,
+  email : {
+        type: String,
+        required: true,
+        unique: true
+    } ,
+  password : {
+      type: String,
+      required: true,
+      select: false 
   },
-  weight : {
-    type: Number,
-    required: true,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
-}, { timestamps: true });
+});
 
-const Note = models.Note || model('Note', NoteSchema);
-export default Note;
+const userModel = model('User', MySchema);
+
+export default userModel;
+
+// import mongoose from 'mongoose';
+// const { Schema, model, models } = mongoose;
+
+// const NoteSchema = new Schema({
+//  name: {
+//     type: String,
+//     required: true,
+//     maxlength: 50
+//   },
+//   email : {
+//         type: String,
+//         required: true,
+//         unique: true
+//     } ,
+//   password : {
+//       type: String,
+//       required: true,
+//       select: false 
+//   }
+// }, { timestamps: true });
+
+// const Note = models.Note || model('Note', NoteSchema);
+// export default Note;

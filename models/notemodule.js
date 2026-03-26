e4a5395788fc53b8d897918ec43c3b7030173e6c
Pankaj Chitra <pankajchitra17@gmail.com>
2026-03-26 21:46:55 +0530

    separate schema for notes and users

--- models/notemodule.js
+++ models/notemodule.js
@@ -1,36 +1,9 @@
-import mongoose from 'mongoose';
-const { Schema, model } = mongoose;
-
-
-const MySchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 50
-  },
-  age : {
-    type: Number,
-    required: true,
-  },
-  weight : {
-    type: Number,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-const userModel = model('User', MySchema);
-
-export default userModel;
-
 // import mongoose from 'mongoose';
-// const { Schema, model, models } = mongoose;
+// const { Schema, model } = mongoose;
 
-// const NoteSchema = new Schema({
-//  name: {
+
+// const MySchema = new Schema({
+//   name: {
 //     type: String,
 //     required: true,
 //     maxlength: 50
@@ -43,7 +16,34 @@ export default userModel;
 //     type: Number,
 //     required: true,
 //   },
-// }, { timestamps: true });
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
 
-// const Note = models.Note || model('Note', NoteSchema);
-// export default Note;
+// const userModel = model('User', MySchema);
+
+// export default userModel;
+
+import mongoose from 'mongoose';
+const { Schema, model, models } = mongoose;
+
+const NoteSchema = new Schema({
+ name: {
+    type: String,
+    required: true,
+    maxlength: 50
+  },
+  age : {
+    type: Number,
+    required: true,
+  },
+  weight : {
+    type: Number,
+    required: true,
+  },
+}, { timestamps: true });
+
+const Note = models.Note || model('Note', NoteSchema);
+export default Note;

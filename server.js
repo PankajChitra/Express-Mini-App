c7f893d0a2561f047c77159c88ce2bbe5892b499
Pankaj Chitra <pankajchitra17@gmail.com>
2026-04-03 01:53:57 +0530

    updated main file

--- server.js
+++ server.js
@@ -8,17 +8,8 @@ import errorHandler from './middleware/errorhandler.js';
 import noteRoutes from './routes/routes.js';
 import authRoute from './routes/authRoute.js';
 
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const app = express();
-
-// app.set('views', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(logger);

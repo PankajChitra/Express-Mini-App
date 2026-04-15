0ce49697f194989157ecb9c9fbe135afc39426ea
Pankaj Chitra <pankajchitra17@gmail.com>
2026-04-16 01:48:53 +0530

    vite_api_url

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -6,6 +6,9 @@ const API = axios.create({
   // In production set VITE_API_URL to your backend URL.
   baseURL: import.meta.env.VITE_API_URL ,
 });
+if (!import.meta.env.VITE_API_URL) {
+  throw new Error("VITE_API_URL not set");
+}
 
 // Attach token automatically to every request
 API.interceptors.request.use((config) => {
